a00e25e7e8cc12b2c8d447726e0b83090661b875
KOUSHIKGO4 <koushikgdatta5@gmail.com>
2026-06-10 18:02:25 +0530

    chore: initial project setup and config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GraphCanvas } from "./components/graph/GraphCanvas";
+
+function App() {
+  return (
+    <main className="h-screen bg-[#0b0d0f] text-white">
+      <GraphCanvas />
+    </main>
+  );
+}
+
+export default App;
